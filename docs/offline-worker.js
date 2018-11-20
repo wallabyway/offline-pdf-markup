a2052f7aa0070a4999bc85c23df9ba624b31bd01
Michael Beale <michael.beale@autodesk.com>
2018-11-19 23:04:19 -0800

    updated offline service worker

--- docs/offline-worker.js
+++ docs/offline-worker.js
@@ -1,70 +1,51 @@
-'use strict';
+const CACHE_NAME = 'forge-disconnected-v4';
 
-
-var shellFilesToCache = [
+var STATIC_URLS = [
+	'/',
 	'/index.html',
 	'/skeleton.min.css',
 	'/logo.png',
+
+    'https://developer.api.autodesk.com/modelderivative/v2/viewers/6.*/viewer3D.min.js',
+	'https://developer.api.autodesk.com/modelderivative/v2/viewers/6.*/lmvworker.min.js',
+	'https://developer.api.autodesk.com/modelderivative/v2/viewers/6.*/extensions/PDF/PDF.min.js',
+	'https://developer.api.autodesk.com/modelderivative/v2/viewers/6.*/extensions/PDF/PDF.worker.min.js',
+	'https://developer.api.autodesk.com/modelderivative/v2/viewers/6.*/extensions/Markup/Markup.min.js',
+	'https://developer.api.autodesk.com/modelderivative/v2/viewers/6.*/extensions/Measure/Measure.min.js',
+	'https://developer.api.autodesk.com/modelderivative/v2/viewers/6.*/extensions/Hyperlink/Hyperlink.min.js',
+	'https://developer.api.autodesk.com/modelderivative/v2/viewers/6.*/extensions/CompGeom/CompGeom.min.js',
+	'https://developer.api.autodesk.com/modelderivative/v2/viewers/6.*/res/locales/en/allstrings.json',
+	'https://developer.api.autodesk.com/modelderivative/v2/viewers/6.*/style.min.css',
+	'https://fonts.autodesk.com/ArtifaktElement/WOFF2/Artifakt%20Element%20Regular.woff2',
+
 	'/pdfs/qcad1.pdf',
 	'/pdfs/qcad3.pdf',
 	'/pdfs/wood.pdf',
-	'https://developer.api.autodesk.com/viewingservice/v1/viewers/extensions/Measure/Measure.min.js',
-	'https://developer.api.autodesk.com/viewingservice/v1/viewers/extensions/Hyperlink/Hyperlink.min.js',
-	'https://developer.api.autodesk.com/viewingservice/v1/viewers/extensions/PDF/PDF.min.js',
-	'https://developer.api.autodesk.com/viewingservice/v1/viewers/extensions/PDF/PDF.worker.min.js',
-	'https://developer.api.autodesk.com/viewingservice/v1/viewers/extensions/CompGeom/CompGeom.min.js',
-	'https://developer.api.autodesk.com/viewingservice/v1/viewers/extensions/Markup/Markup.min.js',
-	'https://developer.api.autodesk.com/modelderivative/v2/viewers/6.*/style.min.css',
-    'https://developer.api.autodesk.com/modelderivative/v2/viewers/6.*/viewer3D.min.js',
-	'https://developer.api.autodesk.com/modelderivative/v2/viewers/6.*/lmvworker.min.js',
-	'https://developer.api.autodesk.com/modelderivative/v2/viewers/6.*/res/locales/en/allstrings.json',
-	'https://fonts.autodesk.com/ArtifaktElement/WOFF2/Artifakt%20Element%20Regular.woff2',
 ];
 
-const staticShellPrefix = 'offlineSW-';
-const staticShellCacheId = staticShellPrefix + '01';
+self.addEventListener('install',  e => { e.waitUntil(installAsync())  });
+self.addEventListener('activate', e => { e.waitUntil(activateAsync()) });
+self.addEventListener('fetch',    e => { e.respondWith(fetchAsync(e)) });
+self.addEventListener('error',    e => { console.error('Error event', e ) });
 
+async function installAsync() {
+    self.skipWaiting(); // Replace old service workers without waiting for them to wrap up
+    const cache = await caches.open(CACHE_NAME);
+    await cache.addAll(STATIC_URLS);
+}
 
-self.addEventListener('install', function (event) {
-	event.waitUntil(
-		caches.open(staticShellCacheId).then(function (cache) {
-			return cache.addAll( shellFilesToCache );
-		}).catch(function (error) {
-			console.error(error);
-		})
-	);
-});
+async function activateAsync() {
+    const clients = await self.clients.matchAll({ includeUncontrolled: true });
+    await self.clients.claim();
+}
 
-
-
-self.addEventListener('activate', function (event) {
-	event.waitUntil(
-		caches.keys().then(function (cacheNames) {
-			return Promise.all(
-				cacheNames.map(function (cacheName) {
-					if (cacheName !== staticShellCacheId && cacheName.startsWith( staticShellPrefix )) {
-						return caches.delete(cacheName);
-					}
-				})
-			);
-		})
-	);
-});
-
-
-self.addEventListener('fetch', function (event) {
-	const requestedUrl = event.request.url;
-	event.respondWith(
-		caches.match(event.request).then(function (response) {
-			if (response)
-				return response;
-
-			return fetch(event.request).then(function (response) {
-				return response;
-			}).catch(function (error) {
-				console.error('SERVICEWORKER fetch error: ' + error);
-				return new Response();
-			});
-		})
-	);
-});
+async function fetchAsync(event) {
+    const match = await caches.match(event.request.url, { ignoreSearch: true });
+    if (!match) return fetch(event.request);
+    if (STATIC_URLS.includes(event.request.url)) {
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.add(event.request))
+            .catch((err) => console.log('Cache not updated, but that\'s ok...', err));
+    }
+    return match;
+}
